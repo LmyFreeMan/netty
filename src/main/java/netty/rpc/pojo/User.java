5051570b1ea771d5f4dd3d325ab414631757979d
Lmyfreeman <2515946609@qq.com>
2020-07-25 07:19:13 +0800

    User

---
+++ src/main/java/netty/rpc/pojo/User.java
@@ -0,0 +1,43 @@
+package netty.rpc.pojo;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class User implements Serializable {
+    private String name;
+    private int age;
+    private Date birthday;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", birthday=" + birthday +
+                '}';
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+}
