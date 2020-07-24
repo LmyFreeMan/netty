db9997368022066020602b9387fe191307886c1c
Lmyfreeman <2515946609@qq.com>
2020-07-25 07:21:16 +0800

    RpcClientHandler

---
+++ src/main/java/netty/rpc/client/ClientMsgHandler.java
@@ -0,0 +1,26 @@
+package netty.rpc.client;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import lombok.extern.log4j.Log4j2;
+import netty.rpc.pojo.User;
+
+import java.util.Date;
+
+@Log4j2
+public class ClientMsgHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        User user=new User();
+        user.setAge(18);
+        user.setBirthday(new Date());
+        user.setName("Jack");
+        ctx.channel().writeAndFlush(user);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+       log.debug(msg.toString());
+    }
+}
